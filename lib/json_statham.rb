8393881ce77d04c664b115ab2f45396267eaa2b3
Victor <authiatv@gmail.com>
2023-02-05 12:05:21 +0100

    Initial jason

---
+++ lib/json_statham.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "json_statham/version"
+
+module JsonStatham
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/json_statham/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JsonStatham
+  VERSION = "0.1.0"
+end
